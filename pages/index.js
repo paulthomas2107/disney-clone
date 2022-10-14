83af6693291716e147c9adabe87d6a34f8470411
Paul Thomas <paulthomas2107@gmail.com>
2022-10-14 17:22:42 +0100

    More

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
-export default function Home() {
+import {gql, GraphQLClient} from 'graphql-request'
+
+
+const Home = ()  => {
   return (
     <div>
       Hello Paul !
     </div>
   )
 }
+
+export default Home
